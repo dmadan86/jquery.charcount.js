b4d511720fc8d08715f72f16bb8198ba171d5354
Madan <dmadan86@gmail.com>
2013-08-18 10:51:20 +0530

    Updated the code to check the state on init

--- src/jquery.charcount.js
+++ src/jquery.charcount.js
@@ -1,7 +1,49 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Madan
- * Date: 8/18/13
- * Time: 10:02 AM
- * To change this template use File | Settings | File Templates.
+/*
+ * 	Character Count Plugin - jQuery plugin
+ * 	Dynamic character count for text areas and input fields
+ *	written by Madan
+ *	http://dmadan.in/plugin/jquery.charcount
+ *
+ *	Built for jQuery library
+ *	http://jquery.com
+ *
  */
+(function($) {
+    function CharCount(obj, options){
+        var config = $.extend($.fn.charCount.defaults, options),
+            text,
+            callback = function(){
+                calculate();
+            };
+        text = $("<" + config.counterElement + "/>");
+        obj = $(obj).keyup(callback).change(callback).after(text);
+
+        function calculate(){
+            var len = obj.val().length;
+            var available = config.maxCount - len;
+            if(len >= config.minCount && len <= config.maxCount) {
+                obj.removeClass(config.cssExceeded).addClass(config.cssCorrect);
+            } else {
+                obj.removeClass(config.cssCorrect).addClass(config.cssExceeded);
+            }
+            text.html(config.counterText + available);
+        }
+        calculate();
+    }
+
+    $.fn.charCount = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "charcount")) {
+                $.data(this, "charcount", new CharCount(this, options))
+            }
+        })
+    };
+    $.fn.charCount.defaults = {
+        minCount: 10,
+        maxCount: 50,
+        cssExceeded: 'exceeded',
+        cssCorrect: "correct",
+        counterElement: "span",
+        counterText: "Total Count: "
+    };
+}(jQuery));
