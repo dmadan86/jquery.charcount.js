597f2fbb78e2ce3282f8069b0e9d5d797a34307d
Madan <dmadan86@gmail.com>
2013-08-18 10:40:36 +0530

    Initial Commit for Charcount

---
+++ GruntFile.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Madan
+ * Date: 8/18/13
+ * Time: 9:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ src/jquery.charcount.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Madan
+ * Date: 8/18/13
+ * Time: 10:02 AM
+ * To change this template use File | Settings | File Templates.
+ */
